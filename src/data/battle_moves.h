e0c7581c914799f9de22b4014ef5bbe069d534b0
Toby <56541253+Toby313@users.noreply.github.com>
2020-03-22 20:21:07 +0100

    Buffed Triple Kick

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -2342,7 +2342,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
     [MOVE_TRIPLE_KICK] =
     {
         .effect = EFFECT_TRIPLE_KICK,
-        .power = 10,
+        .power = 15,
         .type = TYPE_FIGHTING,
         .accuracy = 90,
         .pp = 10,
