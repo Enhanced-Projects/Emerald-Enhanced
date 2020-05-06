b86b3f6483753d103cecbe835f6bae67afd2c214
Toby <56541253+Toby313@users.noreply.github.com>
2020-05-06 03:01:49 +0200

    Forgot Stomp

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -332,7 +332,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .secondaryEffectChance = 30,
         .target = MOVE_TARGET_SELECTED,
         .priority = 0,
-        .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_SHEER_FORCE_BOOST | FLAG_DMG_MINIMIZE,
+        .flags = FLAG_MAKES_CONTACT | FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_SHEER_FORCE_BOOST | FLAG_DMG_MINIMIZE | FLAG_KICK_BOOST,
         .split = SPLIT_PHYSICAL,
     },
 
