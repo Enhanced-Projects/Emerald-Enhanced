f26d3fbfbe5c47d3b73129503b216400239698f8
Toby <56541253+Toby313@users.noreply.github.com>
2020-05-08 22:25:32 +0200

    Made Hydro Cannon affected by Mega Launcher

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -4324,7 +4324,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
         .secondaryEffectChance = 0,
         .target = MOVE_TARGET_SELECTED,
         .priority = 0,
-        .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGSROCK_AFFECTED,
+        .flags = FLAG_PROTECT_AFFECTED | FLAG_MIRROR_MOVE_AFFECTED | FLAG_KINGSROCK_AFFECTED | FLAG_MEGA_LAUNCHER_BOOST,
         .split = SPLIT_SPECIAL
     },
 
