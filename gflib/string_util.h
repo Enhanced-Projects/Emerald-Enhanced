072cfb835ed189b805b25b4a2ee0097fcf209e81
Ryuhouji <bcummans@hotmail.com>
2020-06-05 04:32:20 -0400

    Added 3 new global string vars

--- gflib/string_util.h
+++ gflib/string_util.h
@@ -5,6 +5,9 @@ extern u8 gStringVar1[];
 extern u8 gStringVar2[];
 extern u8 gStringVar3[];
 extern u8 gStringVar4[];
+extern u8 gRyuStringVar1[];
+extern u8 gRyuStringVar2[];
+extern u8 gRyuStringVar3[];
 
 enum StringConvertMode
 {
