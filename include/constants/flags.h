91b202ac65431761aab951b433d045250e6675cb
Toby313 <tobiashut13@gmail.com>
2020-06-26 19:22:34 +0200

    More Iceland progress, mainly Frosty Forest

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1511,10 +1511,11 @@
 
 #define FLAG_RECEIVED_POKEDEX_FROM_BIRCH            (SYSTEM_FLAGS + 0x84)
 
-#define FLAG_UNUSED_0x8E5                           (SYSTEM_FLAGS + 0x85) // Unused Flag
-#define FLAG_UNUSED_0x8E6                           (SYSTEM_FLAGS + 0x86) // Unused Flag
-#define FLAG_UNUSED_0x8E7                           (SYSTEM_FLAGS + 0x87) // Unused Flag
-#define FLAG_UNUSED_0x8E8                           (SYSTEM_FLAGS + 0x88) // Unused Flag
+//Iceland item flags
+#define FLAG_RYU_FROSTY_FOREST_ENERGY_ROOT          (SYSTEM_FLAGS + 0x85) // Unused Flag
+#define FLAG_RYU_FROSTY_FOREST_BANETTITE            (SYSTEM_FLAGS + 0x86) // Unused Flag
+#define FLAG_RYU_FROSTY_FOREST_DUSK_STONE           (SYSTEM_FLAGS + 0x87) // Unused Flag
+#define FLAG_RYU_FROSTY_FOREST_SPELL_TAG            (SYSTEM_FLAGS + 0x88) // Unused Flag
 #define FLAG_UNUSED_0x8E9                           (SYSTEM_FLAGS + 0x89) // Unused Flag
 #define FLAG_UNUSED_0x8EA                           (SYSTEM_FLAGS + 0x8A) // Unused Flag
 #define FLAG_UNUSED_0x8EB                           (SYSTEM_FLAGS + 0x8B) // Unused Flag
