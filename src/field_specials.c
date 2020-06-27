9889228ea3377c8d40e56c0126a4ab2b084b7903
Ryuhouji <bcummans@hotmail.com>
2020-06-27 03:20:00 -0400

    IV and EV readouts now show all data at the same time, instead of 3 at a time.

--- src/field_specials.c
+++ src/field_specials.c
@@ -4858,57 +4858,56 @@ int CountBadges(void)
     return count;
 }
 
-void RyuIvCheckerDef(void)
+extern const u8 gText_EmptyString2[]; 
+
+void RyuClearStringVars(void)
 {
-    u8 HpIv = 0;
-    u8 DefIv = 0;
-    u8 SpDefIv = 0;
+    StringCopy(gStringVar1, gText_EmptyString2);
+    StringCopy(gStringVar2, gText_EmptyString2);
+    StringCopy(gStringVar3, gText_EmptyString2);
+    StringCopy(gRyuStringVar1, gText_EmptyString2);
+    StringCopy(gRyuStringVar2, gText_EmptyString2);
+    StringCopy(gRyuStringVar2, gText_EmptyString2);
+}
+
+void RyuIvChecker(void)
+{
+    u8 HpIv, AtkIv, DefIv, SpAtkIv, SpDefIv, SpdIv;
+
+    RyuClearStringVars();
     HpIv = GetMonData(&gPlayerParty[0], MON_DATA_HP_IV);
     DefIv = GetMonData(&gPlayerParty[0], MON_DATA_DEF_IV);
     SpDefIv = GetMonData(&gPlayerParty[0], MON_DATA_SPDEF_IV);
-    ConvertIntToDecimalStringN(gStringVar1, HpIv, STR_CONV_MODE_LEADING_ZEROS, 2);
-    ConvertIntToDecimalStringN(gStringVar2, DefIv, STR_CONV_MODE_LEADING_ZEROS, 2);
-    ConvertIntToDecimalStringN(gStringVar3, SpDefIv, STR_CONV_MODE_LEADING_ZEROS, 2);
-}
-
-void RyuIvCheckerOff(void)
-{
-    u8 AtkIv = 0;
-    u8 SpAtkIv = 0;
-    u8 SpeIv = 0;
     AtkIv = GetMonData(&gPlayerParty[0], MON_DATA_ATK_IV);
     SpAtkIv = GetMonData(&gPlayerParty[0], MON_DATA_SPATK_IV);
-    SpeIv = GetMonData(&gPlayerParty[0], MON_DATA_SPEED_IV);
-    ConvertIntToDecimalStringN(gStringVar1, AtkIv, STR_CONV_MODE_LEADING_ZEROS, 2);
-    ConvertIntToDecimalStringN(gStringVar2, SpAtkIv, STR_CONV_MODE_LEADING_ZEROS, 2);
-    ConvertIntToDecimalStringN(gStringVar3, SpeIv, STR_CONV_MODE_LEADING_ZEROS, 2);
+    SpdIv = GetMonData(&gPlayerParty[0], MON_DATA_SPEED_IV);
+    ConvertIntToDecimalStringN(gStringVar1, HpIv, STR_CONV_MODE_LEFT_ALIGN, 3);
+    ConvertIntToDecimalStringN(gStringVar2, AtkIv, STR_CONV_MODE_LEFT_ALIGN, 3);
+    ConvertIntToDecimalStringN(gStringVar3, DefIv, STR_CONV_MODE_LEFT_ALIGN, 3);
+    ConvertIntToDecimalStringN(gRyuStringVar1, SpAtkIv, STR_CONV_MODE_LEFT_ALIGN, 3);
+    ConvertIntToDecimalStringN(gRyuStringVar2, SpDefIv, STR_CONV_MODE_LEFT_ALIGN, 3);
+    ConvertIntToDecimalStringN(gRyuStringVar3, SpdIv, STR_CONV_MODE_LEFT_ALIGN, 3);
 }
 
-void RyuEvCheckerDef(void)
+void RyuEvChecker(void)
 {
-    u8 HpEv = 0;
-    u8 DefEv = 0;
-    u8 SpDefEv = 0;
+    u8 HpEv, AtkEv, DefEv, SpAtkEv, SpDefEv, SpdEv;
+
+    RyuClearStringVars();
     HpEv = GetMonData(&gPlayerParty[0], MON_DATA_HP_EV);
     DefEv = GetMonData(&gPlayerParty[0], MON_DATA_DEF_EV);
     SpDefEv = GetMonData(&gPlayerParty[0], MON_DATA_SPDEF_EV);
-    ConvertIntToDecimalStringN(gStringVar1, HpEv, STR_CONV_MODE_LEADING_ZEROS, 3);
-    ConvertIntToDecimalStringN(gStringVar2, DefEv, STR_CONV_MODE_LEADING_ZEROS, 3);
-    ConvertIntToDecimalStringN(gStringVar3, SpDefEv, STR_CONV_MODE_LEADING_ZEROS, 3);
-}
-
-void RyuEvCheckerOff(void)
-{
-    u8 AtkEv = 0;
-    u8 SpAtkEv = 0;
-    u8 SpeEv = 0;
     AtkEv = GetMonData(&gPlayerParty[0], MON_DATA_ATK_EV);
     SpAtkEv = GetMonData(&gPlayerParty[0], MON_DATA_SPATK_EV);
-    SpeEv = GetMonData(&gPlayerParty[0], MON_DATA_SPEED_EV);
-    ConvertIntToDecimalStringN(gStringVar1, AtkEv, STR_CONV_MODE_LEADING_ZEROS, 3);
-    ConvertIntToDecimalStringN(gStringVar2, SpAtkEv, STR_CONV_MODE_LEADING_ZEROS, 3);
-    ConvertIntToDecimalStringN(gStringVar3, SpeEv, STR_CONV_MODE_LEADING_ZEROS, 3);
+    SpdEv = GetMonData(&gPlayerParty[0], MON_DATA_SPEED_EV);
+    ConvertIntToDecimalStringN(gStringVar1, HpEv, STR_CONV_MODE_LEFT_ALIGN, 3);
+    ConvertIntToDecimalStringN(gStringVar1, AtkEv, STR_CONV_MODE_LEFT_ALIGN, 3);
+    ConvertIntToDecimalStringN(gStringVar2, DefEv, STR_CONV_MODE_LEFT_ALIGN, 3);
+    ConvertIntToDecimalStringN(gRyuStringVar1, SpAtkEv, STR_CONV_MODE_LEFT_ALIGN, 3);
+    ConvertIntToDecimalStringN(gRyuStringVar2, SpDefEv, STR_CONV_MODE_LEFT_ALIGN, 3);
+    ConvertIntToDecimalStringN(gRyuStringVar3, SpdEv, STR_CONV_MODE_LEFT_ALIGN, 3);
 }
+
 
 void RyuIvSetter(void)
 {
@@ -4920,12 +4919,6 @@ void RyuIvSetter(void)
     SetMonData(&gPlayerParty[0], MON_DATA_SPATK_IV, &iv);
     SetMonData(&gPlayerParty[0], MON_DATA_SPDEF_IV, &iv);
     SetMonData(&gPlayerParty[0], MON_DATA_SPEED_IV, &iv);
-}
-
-void RyuLevelSetter(void)
-{
-    u8 level = VarGet(VAR_TEMP_9);
-    SetMonData(&gPlayerParty[0], MON_DATA_LEVEL, &level);
 }
 
 void RyuSetFriendship(void)
