adb7ececd368f519575219a97b65218741eaa597
Ryuhouji <bcummans@hotmail.com>
2020-07-18 13:48:52 -0400

    Custom coded ability capsule

--- src/item_use.c
+++ src/item_use.c
@@ -48,6 +48,7 @@ extern u8 RyuUserModeEVMenu[];
 extern u8 RyuExpBattery[];
 extern u8 Ryu_Forecaster[];
 extern u8 Ryu_SoundTest[];
+extern u8 Ryu_AbilityCapsule[];
 //extern u8 EventScript_PC[];
 extern u8 Ryu_CantUsePcCharging[];
 
@@ -747,8 +748,9 @@ void ItemUseOutOfBattle_Medicine(u8 taskId)
 
 void ItemUseOutOfBattle_AbilityCapsule(u8 taskId)
 {
-    gItemUseCB = ItemUseCB_AbilityCapsule;
-    SetUpItemUseCallback(taskId);
+    SetMainCallback2(CB2_ReturnToField);
+    ScriptContext2_Enable();
+    ScriptContext1_SetupScript(Ryu_AbilityCapsule);
 }
 
 void ItemUseOutOfBattle_ReduceEV(u8 taskId)
