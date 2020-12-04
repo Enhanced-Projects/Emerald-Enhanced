6ecb41d2528aa0122b2d9105f40a5b0d5ba1feaf
Toby313 <tobiashut13@gmail.com>
2020-12-04 01:04:58 +0100

    Gave Brendan swimming sprites

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -62,18 +62,15 @@ const struct SpriteFrameImage gObjectEventPicTable_BrendanAcroBike[] = {
 };
 
 const struct SpriteFrameImage gObjectEventPicTable_BrendanSurfing[] = {
-    overworld_frame(gObjectEventPic_BrendanSurfing, 4, 4, 0),
-    overworld_frame(gObjectEventPic_BrendanSurfing, 4, 4, 2),
-    overworld_frame(gObjectEventPic_BrendanSurfing, 4, 4, 4),
-    overworld_frame(gObjectEventPic_BrendanSurfing, 4, 4, 0),
-    overworld_frame(gObjectEventPic_BrendanSurfing, 4, 4, 0),
-    overworld_frame(gObjectEventPic_BrendanSurfing, 4, 4, 2),
-    overworld_frame(gObjectEventPic_BrendanSurfing, 4, 4, 2),
-    overworld_frame(gObjectEventPic_BrendanSurfing, 4, 4, 4),
-    overworld_frame(gObjectEventPic_BrendanSurfing, 4, 4, 4),
-    overworld_frame(gObjectEventPic_BrendanSurfing, 4, 4, 1),
-    overworld_frame(gObjectEventPic_BrendanSurfing, 4, 4, 3),
-    overworld_frame(gObjectEventPic_BrendanSurfing, 4, 4, 5),
+    overworld_frame(gObjectEventPic_BrendanSurfing, 2, 4, 0),
+    overworld_frame(gObjectEventPic_BrendanSurfing, 2, 4, 1),
+    overworld_frame(gObjectEventPic_BrendanSurfing, 2, 4, 2),
+    overworld_frame(gObjectEventPic_BrendanSurfing, 2, 4, 3),
+    overworld_frame(gObjectEventPic_BrendanSurfing, 2, 4, 4),
+    overworld_frame(gObjectEventPic_BrendanSurfing, 2, 4, 5),
+    overworld_frame(gObjectEventPic_BrendanSurfing, 2, 4, 6),
+    overworld_frame(gObjectEventPic_BrendanSurfing, 2, 4, 7),
+    overworld_frame(gObjectEventPic_BrendanSurfing, 2, 4, 8),
 };
 
 const struct SpriteFrameImage gObjectEventPicTable_BrendanUnderwater[] = {
