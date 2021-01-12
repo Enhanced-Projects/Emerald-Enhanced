91fe23ccc929e30f762472886680a24354014a53
Ryuhouji <bcummans@hotmail.com>
2021-01-12 09:07:56 -0500

    added strings for AP's

--- src/achievement_power.c
+++ src/achievement_power.c
@@ -5,6 +5,37 @@
 #include "constants/items.h"
 #include "item.h"
 
+//platinum powers
+const u8 sPlatinumLabel[] = _("Platinum");
+const u8 sExtendedAPNameSprintBoost[] =     _("Sprint Boost        ...51AP");
+const u8 sExtendedAPNameGlobalRepel[] =     _("Ignore Wild Enc     ...51AP");
+const u8 sExtendedAPNameTrainerRepel[] =    _("Ignore Trainers     ...51AP");
+const u8 sExtendedAPNameEVBoost[] =         _("Double EV Gain      ...51AP");
+
+//gold powers
+const u8 sGoldLabel[] = _("Gold");
+const u8 sExtendedAPNameMobileStatCheck[] = _("Stat Device Access  ...33AP");
+const u8 sExtendedAPNamePDA[] =             _("PDA Device Access   ...33AP");
+
+//silver powers
+const u8 sSilverLabel[] = _("Silver");
+const u8 sExtendedAPNameWinningsBoost[] =   _("Winnings Boost      ...33AP");
+
+//copper powers
+const u8 sCopperLabel[] = _("Copper");
+const u8 sExtendedAPNameCaptureBoost[] =    _("Capture Boost       ...25AP");
+
+//Descriptions
+const u8 sSprintBoostAPDesc[] = _("Allows you to run twice as fast.");
+const u8 sGlobalRepelAPDesc[] = _("Ignore all wild encounters.");
+const u8 sTrainerRepelAPDesc[] = _("Ignore trainer sight.");
+const u8 sEVBoostAPDesc[] = _("Earn double EV's in battle.");
+const u8 sMobileStatCheckAPDesc[] = _("Grants the StatAssist item which\nlets you see your IV's and EV's.");
+const u8 sPDAAPDesc[] = _("Grants the PDA item which\nlets you shop remotely.");
+const u8 sWinningsBoostAPDesc[] = _("Gain 10 percent more money\nfrom winning battles.");
+const u8 sCaptureBoostAPDesc[] = _("Boost your base capture rate by\n5 percent.");
+
+
 const u16 sAP_Requirements[][2] = {
     {AP_SPRINT_BOOST, 51},
     {AP_GLOBAL_REPEL, 51},
