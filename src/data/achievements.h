335889f4a9420ec239b875e897e17c76e28800b1
Ryuhouji <bcummans@hotmail.com>
2021-05-31 04:43:56 -0400

    Put in SAIYAN48's hints

--- src/data/achievements.h
+++ src/data/achievements.h
@@ -1,209 +1,209 @@
 static const u8 sWholeNewWorldAchLabel[] = _("A Whole New World");
-static const u8 sWholeNewWorldAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED}LOCKED.{COLOR DARK_GREY}{SHADOW LIGHT_GREY} Begin your quest.");
+static const u8 sWholeNewWorldAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Begin your quest.");
 static const u8 sWholeNewWorldAchDesc[] = _("Welcome to Emerald Enhanced.\nExplore to your heart's content!\n(Completed tutorial)");
 
 static const u8 sEnhancedBattleAchLabel[] = _("Enhanced Battle");
-static const u8 sEnhancedBattleAchHint[] = _("Do a non-tutorial battle.");
+static const u8 sEnhancedBattleAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Do a non-tutorial battle.");
 static const u8 sEnhancedBattleAchDesc[] = _("You have awakened as a battle master!\n(Finished your first battle)");
 
 static const u8 sNewRegionWhoDisAchLabel[] = _("New Region Who Dis");
-static const u8 sNewRegionWhoDisAchHint[] = _("");
+static const u8 sNewRegionWhoDisAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Talk with someone new.");
 static const u8 sNewRegionWhoDisAchDesc[] = _("You made a friend!\nEverything is better with friends.\n(Befriended someone for the first time)");
 
 static const u8 sAdventureTimeAchLabel[] = _("Adventure Time");
-static const u8 sAdventureTimeAchHint[] = _("");
+static const u8 sAdventureTimeAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Start a new game.");
 static const u8 sAdventureTimeAchDesc[] = _("You started your first quest.\nGood Luck!\n");
 
 static const u8 sTrueLoveAchLabel[] = _("Love at First Sight");
-static const u8 sTrueLoveAchHint[] = _("");
+static const u8 sTrueLoveAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Make a close friend.");
 static const u8 sTrueLoveAchDesc[] = _("You have entered a relationship.\nNow you wonder what you would\ndo without them");
 
 static const u8 sSilentStrongTypeAchLabel[] = _("Silent, Strong type");
-static const u8 sSilentStrongTypeAchHint[] = _("");
+static const u8 sSilentStrongTypeAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Befriend a quiet girl.");
 static const u8 sSilentStrongTypeAchDesc[] = _("The quiet ones usually have the most to say!\n(Romanced Lana)");
 
 static const u8 sChildhoodFriendAchLabel[] = _("A Childhood Friend");
-static const u8 sChildhoodFriendAchHint[] = _("");
+static const u8 sChildhoodFriendAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Rivals can get along.");
 static const u8 sChildhoodFriendAchDesc[] = _("{RIVAL} would follow you off of a cliff.\n(Romanced {RIVAL})");
 
 static const u8 sNerdLoveAchLabel[] = _("Nerd Love");
-static const u8 sNerdLoveAchHint[] = _("");
+static const u8 sNerdLoveAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Programmers can be lonely.");
 static const u8 sNerdLoveAchDesc[] = _("You feel her rubbing off on you.\n(Romanced Lanette)");
 
 static const u8 sFWBAchLabel[] = _("Friends with Benefits");
-static const u8 sFWBAchHint[] = _("");
+static const u8 sFWBAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Heal many times.");
 static const u8 sFWBAchDesc[] = _("You have a secret weapon!\nYour own, personal nurse.\n(Romanced Joy)");
 
 static const u8 sWetnWildAchLabel[] = _("Wet and Wild");
-static const u8 sWetnWildAchHint[] = _("");
+static const u8 sWetnWildAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Aquacious amore.");
 static const u8 sWetnWildAchDesc[] = _("You found a girl who loves to get wet.\n(Romanced Shelly)");
 
 static const u8 sFieryPassionAchLabel[] = _("Fiery Passion");
-static const u8 sFieryPassionAchHint[] = _("");
+static const u8 sFieryPassionAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Lava love.");
 static const u8 sFieryPassionAchDesc[] = _("She's blazing hot!\n(Romanced Courtney)");
 
 static const u8 sTrueEndingAchLabel[] = _("The True Ending");
-static const u8 sTrueEndingAchHint[] = _("");
+static const u8 sTrueEndingAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Chick magnet!");
 static const u8 sTrueEndingAchDesc[] = _("You're quite the lady killer, eh?\n(Got the Harem ending)");
 
 static const u8 sEternalDamnationLabel[] = _("Eternal Damnation");
-static const u8 sEternalDamnationHint[] = _("");
+static const u8 sEternalDamnationHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Another burning love?");
 static const u8 sEternalDamnationAchDesc[] = _("And thus, a new monarchy was born!\n(Completed Magma Alt Romance line)");
 
 static const u8 sHuntingTripAchLabel[] = _("Hunting Trip");
-static const u8 sHuntingTripAchHint[] = _("");
+static const u8 sHuntingTripAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}A burning death.");
 static const u8 sHuntingTripAchDesc[] = _("There's something strangely cathartic about\nhunting a person down and finishing the job.\n(Completed the Magma Alt line)");
 
 static const u8 sThroughTheFireAchLabel[] = _("Through the Fire and Flames");
-static const u8 sThroughTheFireAchHint[] = _("");
+static const u8 sThroughTheFireAchHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Volcanic desire.");
 static const u8 sThroughTheFireAchDesc[] = _("You made a deal with the devil.\nAll who stand in your way will perish.\n(Completed Magma main Romance line)");
 
 static const u8 sHeatedArgumentLabel[] = _("A Heated Argument");
-static const u8 sHeatedArgumentHint[] = _("");
+static const u8 sHeatedArgumentHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}The fire tale.");
 static const u8 sHeatedArgumentAchDesc[] = _("You could say he... went up in flames.\n(Completed Magma Main line)");
 
 static const u8 sEyeOfTheStormLabel[] = _("Eye of the Storm");
-static const u8 sEyeOfTheStormHint[] = _("");
+static const u8 sEyeOfTheStormHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Love of water.");
 static const u8 sEyeOfTheStormAchDesc[] = _("You travelled into the eye of the storm,\nand lived to tell the tale.\n(Completed Aqua Romance line)");
 
 static const u8 sWaterLoggedLabel[] = _("Water Logged");
-static const u8 sWaterLoggedHint[] = _("");
+static const u8 sWaterLoggedHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Balanced waves.");
 static const u8 sWaterLoggedAchDesc[] = _("You're a little wet and very disappointed.\nAt least you didn't go to jail!\n(Completed Aqua Neutral line)");
 
 static const u8 sAstronomicalLabel[] = _("Astronomical!");
-static const u8 sAstronomicalHint[] = _("");
+static const u8 sAstronomicalHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Science = knowledge.");
 static const u8 sAstronomicalAchDesc[] = _("The possibilities are {SHADOW GREEN}literally{SHADOW LIGHT_GREY} endless!\n(Completed Devon Scientist line)");
 
 static const u8 sDejaVuLabel[] = _("Deja Vu?");
-static const u8 sDejaVuHint[] = _("");
+static const u8 sDejaVuHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}End of the ends.");
 static const u8 sDejaVuDesc[] = _("Hey! a lot of this looks familiar!\n(Got every main quest ending possible)");
 
 static const u8 sNTMOLabel[] = _("Never tell me the odds");
-static const u8 sNTMOHint[] = _("");
-static const u8 sNTMODesc[] = _("You really don't wanna know...\n(Captured a legendary with a Poke Ball)");
+static const u8 sNTMOHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}An impossible capture?");
+static const u8 sNTMODesc[] = _("You really don't wanna know...\n(Captured a legendary with a Beast Ball)");
 
 static const u8 sPokemonMasterLabel[] = _("Pokemon Master");
-static const u8 sPokemonMasterHint[] = _("");
+static const u8 sPokemonMasterHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Dex-god.");
 static const u8 sPokemonMasterDesc[] = _("You did what Ash couldn't.\n(Own at least 386 different pokemon)");
 
 static const u8 sCorporateShillLabel[] = _("Corporate Shill");
-static const u8 sCorporateShillHint[] = _("");
+static const u8 sCorporateShillHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Blue-collar.");
 static const u8 sCorporateShillAchDesc[] = _("Stocks, lobbying, tax evasion, oh my!\n(Completed Devon Corporate line)");
 
 static const u8 sMasterBreederLabel[] = _("The birds and the bees");
-static const u8 sMasterBreederHint[] = _("");
+static const u8 sMasterBreederHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Eggscelent!");
 static const u8 sMasterBreederDesc[] = _("You know a thing or two about mating.\n(Hatched at least 100 eggs)");
 
 static const u8 sAscendedLabel[] = _("Ascended");
-static const u8 sAscendedHint[] = _("");
+static const u8 sAscendedHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Smite the god.");
 static const u8 sAscendedDesc[] = _("You are the god of this world, now.\n(Defeated Overlord Ryu with no faints)");
 
 static const u8 sEvilIncarnateLabel[] = _("Evil Incarnate");
-static const u8 sEvilIncarnateHint[] = _("");
+static const u8 sEvilIncarnateHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Satan's pet.");
 static const u8 sEvilIncarnateDesc[] = _("You're the sign of the Apocalypse!\n(KO'd 666 or more pokemon)");
 
 static const u8 s1337Label[] = _("1337");
-static const u8 s1337Hint[] = _("");
+static const u8 s1337Hint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}L33T Timez.");
 static const u8 s1337Desc[] = _("y0u 4r3 7h3 l337357 0f 7h3m 4ll\n(Completed the game in 13:37 or less)");
 
 static const u8 sYouDiedLabel[] = _("YOU DIED");
-static const u8 sYouDiedHint[] = _("");
+static const u8 sYouDiedHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Loser!");
 static const u8 sYouDiedDesc[] = _("Your team fainted, and you blacked out.\nThis isn't child's play. Get better.\n(Lost a fight)");
 
 static const u8 sWastedLabel[] = _("Wasted");
-static const u8 sWastedHint[] = _("");
+static const u8 sWastedHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Difficult death.");
 static const u8 sWastedDesc[] = _("You and your team died in a previous life.\nDo better this time!\n(Lost a nuzlocke or hardcore run)");
 
 static const u8 sFitnessGuruLabel[] = _("Fitness Guru");
-static const u8 sFitnessGuruHint[] = _("");
-static const u8 sFitnessGuruDesc[] = _("You're well on your way to a perfect\ntraining regimen.\n(Fully trained EV's in a stat)");
+static const u8 sFitnessGuruHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Stat Master.");
+static const u8 sFitnessGuruDesc[] = _("You're well on your way to a perfect\ntraining regimen.\n(Fully trained EVs in a stat)");
 
 static const u8 sGreenThumbLabel[] = _("Green Thumb");
-static const u8 sGreenThumbHint[] = _("");
+static const u8 sGreenThumbHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Gardening can be Therapeutic.");
 static const u8 sGreenThumbDesc[] = _("You're a practiced berry farmer.\n(Planted 50 or more berries)");
 
 static const u8 sHoennChampionLabel[] = _("Hoenn Champion");
-static const u8 sHoennChampionHint[] = _("");
+static const u8 sHoennChampionHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}E4 ruler.");
 static const u8 sHoennChampionDesc[] = _("You've become the champion of Hoenn.\nCongratulations!");
 
 static const u8 sLNSULabel[] = _("Leave No Stone Unturned");
-static const u8 sLNSUHint[] = _("");
+static const u8 sLNSUHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Explore the world!");
 static const u8 sLNSUDesc[] = _("You've visited every landmark location\nin the game.\nColumbus would be proud!");
 
 static const u8 sOverkill2xLabel[] = _("No Kill Like Overkill");
-static const u8 sOverkill2xHint[] = _("");
+static const u8 sOverkill2xHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Double or nothing?");
 static const u8 sOverkill2xDesc[] = _("You dealt more than twice a target's\nhealth in damage.");
 
 static const u8 sOverkill4xLabel[] = _("Additive Overkill");
-static const u8 sOverkill4xHint[] = _("");
+static const u8 sOverkill4xHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Quad-crush.");
 static const u8 sOverkill4xDesc[] = _("You dealt more than quadruple a target's\nhealth in damage. Ouch!");
 
 static const u8 sOverkill10xLabel[] = _("Multiplicative Overkill");
-static const u8 sOverkill10xHint[] = _("");
+static const u8 sOverkill10xHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Decimate your enemies!");
 static const u8 sOverkill10xDesc[] = _("You really smashed a target for\nmore than ten times their health!\nYikes!");
 
 static const u8 sOverkill20xLabel[] = _("Exponential Overkill");
-static const u8 sOverkill20xHint[] = _("");
+static const u8 sOverkill20xHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}2x Decimation?!");
 static const u8 sOverkill20xDesc[] = _("You atomized a target for more than\nTWENTY TIMES their health! Hot damn!");
 
 static const u8 sWellTravelledLabel[] = _("Well Travelled");
-static const u8 sWellTravelledHint[] = _("");
+static const u8 sWellTravelledHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Urban undertaker.");
 static const u8 sWellTravelledDesc[] = _("You've visited every single city\nin the game!\nTime to set up trade routes!");
 
 static const u8 sMentorLabel[] = _("Mentor");
-static const u8 sMentorHint[] = _("");
+static const u8 sMentorHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Mentor = teacher.");
 static const u8 sMentorDesc[] = _("You finished mentoring a girl from\nthe School Kids faction.\nWell done, teach!");
 
 static const u8 sUndertakerLabel[] = _("Undertaker");
-static const u8 sUndertakerHint[] = _("");
+static const u8 sUndertakerHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}RIP ghosts.");
 static const u8 sUndertakerDesc[] = _("You laid an ailing spirit to rest.\nMimi is eternally grateful!");
 
 static const u8 sFieldMedicLabel[] = _("Field Medic");
-static const u8 sFieldMedicHint[] = _("");
+static const u8 sFieldMedicHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Helpful healer.");
 static const u8 sFieldMedicDesc[] = _("You healed another trainer's Pokemon.\nYou're practically an Army doctor!");
 
 static const u8 sPlotArmorLabel[] = _("Plot Armor");
-static const u8 sPlotArmorHint[] = _("");
+static const u8 sPlotArmorHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Face extreme adversity... and win.");
 static const u8 sPlotArmorDesc[] = _("Even the previous Champion couldn't beat you!\nThe game's storyline is on your side!\n(Won against Steven in the Museum)");
 
 static const u8 sTheExplorerLabel[] = _("The Explorer");
-static const u8 sTheExplorerHint[] = _("");
+static const u8 sTheExplorerHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Naturalistic.");
 static const u8 sTheExplorerDesc[] = _("You have become one with Nature.\n(175+ standing with the Naturalists.)");
 
 static const u8 sTheProtegeLabel[] = _("The Protege");
-static const u8 sTheProtegeHint[] = _("");
+static const u8 sTheProtegeHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Fav teacher.");
 static const u8 sTheProtegeDesc[] = _("As a Teacher, you learn more about yourself.\n(175+ standing with the Students.)");
 
 static const u8 sTheEmperorLabel[] = _("The Emperor");
-static const u8 sTheEmperorHint[] = _("");
+static const u8 sTheEmperorHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Noble blood.");
 static const u8 sTheEmperorDesc[] = _("You have proven your value to others,\nbut what of your self worth?\n(175+ standing with the Nobles.)");
 
 static const u8 sTheGuardianLabel[] = _("The Guardian");
-static const u8 sTheGuardianHint[] = _("");
+static const u8 sTheGuardianHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Fan-fav.");
 static const u8 sTheGuardianDesc[] = _("You come to the aid of others, even at\ngreat cost to yourself.\n(175+ standing with the Pokefans.)");
 
 static const u8 sTheMagicianLabel[] = _("The Magician");
-static const u8 sTheMagicianHint[] = _("");
+static const u8 sTheMagicianHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Spell-lord.");
 static const u8 sTheMagicianDesc[] = _("You align yourself with natural magics.\nWatch, lest ye be absorbed by the powers\nthat be. (175+ standing with Outcasts.)");
 
 static const u8 sTheTradesmanLabel[] = _("The Tradesman");
-static const u8 sTheTradesmanHint[] = _("");
+static const u8 sTheTradesmanHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Pure-pro.");
 static const u8 sTheTradesmanDesc[] = _("Your skill may be unrivaled, but what of\nyour compassion?\n(175+ standing with the Professionals.)");
 
 static const u8 sTheWarriorLabel[] = _("The Warrior");
-static const u8 sTheWarriorHint[] = _("");
+static const u8 sTheWarriorHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Olympian.");
 static const u8 sTheWarriorDesc[] = _("Your unbridled power is impressive, but\nhow does your intelligence stack?\n(175+ standing with the Athletes.)");
 
 static const u8 sTheUniversalistLabel[] = _("The Universalist");
-static const u8 sTheUniversalistHint[] = _("");
+static const u8 sTheUniversalistHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Story path taker.");
 static const u8 sTheUniversalistDesc[] = _("You know of the plights of all walks\nof life. You are wise.\n(Obtained all faction based achievements.)");
 
 static const u8 sLoremasterLabel[] = _("Lore Master");
-static const u8 sLoremasterHint[] = _("");
+static const u8 sLoremasterHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}Quest completer.");
 static const u8 sLoremasterDesc[] = _("You have been everywhere. Done everything.\nAnd yet, you still want more!\n(Obtained all Quest Completion Achievements.)");
 
 static const u8 sTouristLabel[] = _("Tourist");
-static const u8 sTouristHint[] = _("");
+static const u8 sTouristHint[] = _("{COLOR LIGHT_RED}{SHADOW RED} Locked.  {COLOR DARK_GREY}{SHADOW LIGHT_GREY}You could fill out a map!");
 static const u8 sTouristDesc[] = _("There is no place you haven't been.\nMaybe you should become a Cartographer?\n(Obtained all Exploration Achievements.)");
 
 
