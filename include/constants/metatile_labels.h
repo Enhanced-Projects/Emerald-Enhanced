d4f112c0e2e7657376a9a744c4190267b3231c53
Toby313 <tobiashut13@gmail.com>
2021-06-12 01:44:01 +0200

    Designed houses for Basalt Burg

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -131,6 +131,9 @@
 #define METATILE_Contest_FloorShadow         0x261
 #define METATILE_Contest_WallShadow          0x221
 
+// gTileset_DarkWorld
+#define METATILE_DarkWorld_Door_DarkWorld  0x029
+
 // gTileset_Dewford
 #define METATILE_Dewford_Door              0x225
 #define METATILE_Dewford_Door_BattleTower  0x25D
