8a6c13203b14e804c85cd8d8e4e22d58cd7b2c10
MCboy <atasro2@gmail.com>
2021-06-22 21:27:26 +0430

    make followers use 16-bit gfxId

--- src/scrcmd.c
+++ src/scrcmd.c
@@ -2301,7 +2301,7 @@ bool8 ScrCmd_warpsootopolislegend(struct ScriptContext *ctx)
 
 bool8 ScrCmd_createfollower(struct ScriptContext *ctx)
 {
-    u8 graphicsId = ScriptReadByte(ctx);
+    u16 graphicsId = ScriptReadHalfword(ctx);
     const void *script = (const void *)ScriptReadWord(ctx);
     u8 direction = ScriptReadByte(ctx);
     CreateFollowerObjectEvent(graphicsId, script, direction);
