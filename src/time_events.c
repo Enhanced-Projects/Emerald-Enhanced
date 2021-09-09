c70e54f7f6a92904216656d13b85ecfd41248aec
Ryuhouji <bcummans@hotmail.com>
2021-09-09 06:21:06 -0400

    Force enable mirage island permanently with game clear

--- src/time_events.c
+++ src/time_events.c
@@ -41,13 +41,8 @@ void UpdateMirageRnd(u16 days)
 
 bool8 IsMirageIslandPresent(void)
 {
-    u16 rnd = GetMirageRnd() >> 16;
-    int i;
-
-    for (i = 0; i < PARTY_SIZE; i++)
-        if (GetMonData(&gPlayerParty[i], MON_DATA_SPECIES) && (GetMonData(&gPlayerParty[i], MON_DATA_PERSONALITY) & 0xFFFF) == rnd)
-            return TRUE;
-
+    if (FlagGet(FLAG_SYS_GAME_CLEAR) == TRUE)
+        return TRUE;
     return FALSE;
 }
 
