e59c4c37eb80844599d8ac76fb07685cbd830057
Ryuhouji <bcummans@hotmail.com>
2021-10-10 19:17:38 -0400

    added notification system file

---
+++ src/notification_system.c
@@ -0,0 +1,10 @@
+#include "global.h"
+#include "ach_atlas.h"
+#include "RyuRealEstate.h"
+#include "string.h"
+#include "text_window.h"
+#include "data.h"
+
+const u16 gNotificationWindowTiles[] = INCBIN_U16("graphics/notification/notify_window_tiles.4bpp");
+const u16 gNotificationWindowMap[] = INCBIN_U16("graphics/notification/notify_window_map.bin");
+const u8 gNotificationCategorySprite[] = INCBIN_U8("graphics/notification/notification_category.4bpp");
