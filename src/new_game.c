5d514a3cfaa94419c6ca3763f1b1393da469efcc
Ryuhouji <bcummans@hotmail.com>
2022-03-05 01:24:53 -0500

    fixed may showing up when she shouldn't

--- src/new_game.c
+++ src/new_game.c
@@ -314,6 +314,7 @@ void NewGameInitData(void)
     FlagSet(FLAG_RYU_HIDE_VERDANTURF_WALLY);
     FlagSet(FLAG_RYU_HIDE_RT_WALLY);
     FlagSet(FLAG_RYU_HIDE_PBC_HOME_WALLY);
+    FlagSet(FLAG_RYU_HIDE_PETALBURG_MAY);
     //vars
     VarSet(VAR_RYU_GCMS_SPECIES, 0);
     VarSet(VAR_RYU_GCMS_VALUE, 0);
