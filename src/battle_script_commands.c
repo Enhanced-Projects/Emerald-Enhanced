Enhanced-Projects/Emerald-Enhanced

commit a0424f3a581f7152e69ae9d546f9916854f993a5

add blizzard battle config

src/battle_script_commands.c
@@ -1565,17 +1565,24 @@ static bool32 AccuracyCalcHelper(u16 move)
         return TRUE;
     }
 
-    if ((WEATHER_HAS_EFFECT &&
-            ((IsBattlerWeatherAffected(gBattlerTarget, WEATHER_RAIN_ANY) && (gBattleMoves[move].effect == EFFECT_THUNDER || gBattleMoves[move].effect == EFFECT_HURRICANE))))
-     || (gBattleMoves[move].effect == EFFECT_VITAL_THROW)
-     || (gBattleMoves[move].accuracy == 0)
-     || ((B_MINIMIZE_DMG_ACC >= GEN_6) && (gStatuses3[gBattlerTarget] & STATUS3_MINIMIZED) && (gBattleMoves[move].flags & FLAG_DMG_MINIMIZE)))
+    if (WEATHER_HAS_EFFECT)
     {
-        JumpIfMoveFailed(7, move);
-        return TRUE;
-    }
+        if ((IsBattlerWeatherAffected(gBattlerTarget, WEATHER_RAIN_ANY) && (gBattleMoves[move].effect == EFFECT_THUNDER || gBattleMoves[move].effect == EFFECT_HURRICANE)))
+        {
+            // thunder/hurricane ignore acc checks in rain unless target is holding utility umbrella
+            JumpIfMoveFailed(7, move);
+            return TRUE;
+        }
+    #if B_BLIZZARD_HAIL >= GEN_4
+        else if ((gBattleWeather & WEATHER_HAIL_ANY) && (move == MOVE_BLIZZARD))
+        {
+            JumpIfMoveFailed(7, move);
+            return TRUE;
+        }
+    #endif
 
     return FALSE;
+    }
 }
 
 u32 GetTotalAccuracy(u32 battlerAtk, u32 battlerDef, u32 move)

include/constants/battle_config.h
@@ -132,6 +132,8 @@
 #define B_PARALYZE_ELECTRIC         GEN_7 // In Gen6+, Electric type Pokémon can't be paralyzed.
 #define B_POWDER_GRASS              GEN_7 // In Gen6+, Grass type Pokémon are immune to powder and spore moves.
 
+#define B_BLIZZARD_HAIL             GEN_7 // In Gen4+, Blizzard bypasses accuracy checks if it's hailing.
+
 // Animation Settings
 #define B_NEW_SWORD_PARTICLE            TRUE    // update swords dance particle
 #define B_NEW_LEECH_SEED_PARTICLE       TRUE    //update leech seed's animation particle

commit cfbac132635269eac3d142a0ed17ff592aa85fcc

revert accuracy change and fix mistake in AccuracyCalcHelper caused by a0424f3a58

src/battle_script_commands.c
@@ -1580,9 +1580,9 @@ static bool32 AccuracyCalcHelper(u16 move)
             return TRUE;
         }
     #endif
-
-    return FALSE;
     }
+    
+    return FALSE;
 }
 
 u32 GetTotalAccuracy(u32 battlerAtk, u32 battlerDef, u32 move)
@@ -1654,12 +1654,6 @@ u32 GetTotalAccuracy(u32 battlerAtk, u32 battlerDef, u32 move)
         calc = (calc * (100 + atkParam)) / 100;
     else if (atkHoldEffect == HOLD_EFFECT_ZOOM_LENS && GetBattlerTurnOrderNum(battlerAtk) > GetBattlerTurnOrderNum(battlerDef));
         calc = (calc * (100 + atkParam)) / 100;
-        
-// HACK: This makes all moves with an accuracy of 0 always hit. 
-// This is a band aid to all 'always hit' moves universally missing.
-// Remove when latter issue is resolved.
-    if (gBattleMoves[move].accuracy == 0)
-        calc += 100;
 
     return calc;
 }
@@ -1671,8 +1665,8 @@ static void Cmd_accuracycheck(void)
     if (move == ACC_CURR_MOVE)
         move = gCurrentMove;
 
-    //if (gBattleMoves[move].accuracy == 0)
-        //gBattlescriptCurrInstr += 7;
+    if (gBattleMoves[move].accuracy == 0)
+        gBattlescriptCurrInstr += 7;
 
     if (move == NO_ACC_CALC_CHECK_LOCK_ON)
     {