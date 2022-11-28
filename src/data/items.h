e4b36a7a2394c9eea4fd258b459885cd591aeb4d
Toby <tobiashut13@gmail.com>
2022-11-28 03:08:24 +0100

    Updated drink item prices

--- src/data/items.h
+++ src/data/items.h
@@ -630,7 +630,7 @@ const struct Item gItems[] =
     {
         .name = _("Moomoo Milk"),
         .itemId = ITEM_MOOMOO_MILK,
-        .price = 750,
+        .price = 400,
         .holdEffectParam = 100,
         .description = sMoomooMilkDesc,
         .pocket = POCKET_MEDICINE,
@@ -843,7 +843,7 @@ const struct Item gItems[] =
     {
         .name = _("Berry Juice"),
         .itemId = ITEM_BERRY_JUICE,
-        .price = 1000,
+        .price = 100,
         .holdEffect = HOLD_EFFECT_RESTORE_HP,
         .holdEffectParam = 20,
         .description = sBerryJuiceDesc,
