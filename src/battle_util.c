86bd8b5d1b9e999ad7942bccae97060832e9ec5b
Toby <tobiashut13@gmail.com>
2022-12-31 03:33:25 +0100

    Prevent Voodoo from working when KO'd

--- src/battle_util.c
+++ src/battle_util.c
@@ -4840,6 +4840,7 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u8 ability, u8 special, u16 moveA
             if (!(gMoveResultFlags & MOVE_RESULT_NO_EFFECT)
              && gBattleMons[gBattlerAttacker].hp != 0
              && TARGET_TURN_DAMAGED
+             && gBattleMons[gBattlerTarget].hp != 0
              && ((gBattleStruct->hpBefore[gBattlerTarget]) > (gBattleMons[gBattlerTarget].maxHP / 2))
              && ((gBattleMons[gBattlerTarget].hp) < (gBattleMons[gBattlerTarget].maxHP / 2))
              && !(gBattleMons[gBattlerAttacker].status2 & STATUS2_CURSED))
