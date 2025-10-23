32966af375cebd47093e85c1739c0cc546c3d80a
B L U E <116829412+This-Is-BLUE@users.noreply.github.com>
2025-10-23 10:20:00 -0600

    Add tutor moves for SPECIES_ARCEUS
    Arceus now has tutor support

--- src/data/pokemon/tutor_learnsets.h
+++ src/data/pokemon/tutor_learnsets.h
@@ -4413,7 +4413,14 @@ static const u32 sTutorLearnsets[] =
 
     [SPECIES_SHAYMIN]       = TUTOR_LEARNSET(0),
 
-    [SPECIES_ARCEUS]        = TUTOR_LEARNSET(0),
+    [SPECIES_ARCEUS]        = TUTOR_LEARNSET(TUTOR(MOVE_DRACO_METEOR)
+											| TUTOR(MOVE_MAGIC_COAT)
+											| TUTOR(MOVE_TRICK)
+											| TUTOR(MOVE_METRONOME)
+											| TUTOR(MOVE_ZEN_HEADBUTT)
+											| TUTOR(MOVE_HEAT_WAVE)
+											| TUTOR(MOVE_LIQUIDATION)
+											| TUTOR(MOVE_SIGNAL_BEAM),
 
     [SPECIES_VICTINI]       = TUTOR_LEARNSET(TUTOR(MOVE_BOUNCE)
 											| TUTOR(MOVE_DOUBLE_EDGE)
@@ -4425,6 +4432,7 @@ static const u32 sTutorLearnsets[] =
 											| TUTOR(MOVE_HELPING_HAND)
 											| TUTOR(MOVE_ENDURE)
 											| TUTOR(MOVE_THUNDER_PUNCH)
+											| TUTOR(MOVE_LIQUIDATION)
 											| TUTOR(MOVE_FIRE_PUNCH)
 											| TUTOR(MOVE_SIGNAL_BEAM)),
 
