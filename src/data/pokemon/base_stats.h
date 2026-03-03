4dbf4728e372a9f4213c14879084348249813d65
B L U E <116829412+This-Is-BLUE@users.noreply.github.com>
2026-03-03 12:02:31 -0600

    Change Arceus ability from Infiltrator to Protean

--- src/data/pokemon/base_stats.h
+++ src/data/pokemon/base_stats.h
@@ -12991,7 +12991,7 @@ const struct BaseStats gBaseStats[] =
         .growthRate = GROWTH_SLOW,
         .eggGroup1 = EGG_GROUP_UNDISCOVERED,
         .eggGroup2 = EGG_GROUP_UNDISCOVERED,
-        .abilities = {ABILITY_INFILTRATOR, ABILITY_NONE},
+        .abilities = {ABILITY_PROTEAN, ABILITY_NONE},
         .noFlip = FALSE,
         .bodyColor = BODY_COLOR_WHITE,
     },
